66853d8639b54d4a3f89150091fffeca767500b3
demonza <tmendonca@live.com.pt>
2026-03-27 18:33:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ if (missing.length) {
 }
 
 const express = require('express');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const cors    = require('cors');
 const helmet  = require('helmet');
 const pool    = require('./db/pool');
