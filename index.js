8ce031842fc2ac5ab5f4c1b8a23b4d822dbea285
demonza <tmendonca@live.com.pt>
2026-03-27 18:35:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ if (missing.length) {
 }
 
 const express = require('express');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 const cors    = require('cors');
 const helmet  = require('helmet');
 const pool    = require('./db/pool');
@@ -58,6 +56,9 @@ const corsOptions = {
 };
 
 // ─── MIDDLEWARE ───────────────────────────────────────
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(helmet({
   contentSecurityPolicy: false
 }));
